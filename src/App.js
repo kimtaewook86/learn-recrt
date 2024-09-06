829f54c3279ac6fbd31520f72516b4e6f3b3563f
kimtaewook86 <kimtaewook86@naver.com>
2024-09-06 17:22:01 +0900

    로직컬 오퍼레이터 학습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 function App() {
-  return (
-    <div >Hello, world</div>
-  );
+
+  console.log("App.js");
+
+  const name = '김태욱';
+  const age = 39;
+
+  // 안녕하세요. 김태욱님! 나이가 39세 이시네요
+  // 환영합니다!
+
+  /**
+   * logical opaerators
+   * 단축 평가 논리 계산법
+   */
+
+  const user = {
+    isLoggedIn: false,
+    role: "admon", // 'guest' 'user'
+  };
+
+  // 사용자가 로그인 한 상태이면서, 역할이 'admin'인 경우엔 "관리자 페이지 접근 가능",아니면 "관리자 페이지 접근 불가능"
+  if (user.isLoggedIn && user.role === 'admin') {
+    console.log("관리자 페이지 접근 가능")
+  } else {
+    console.log("관리자 페이지 접근 불가능")
+  }
+  // view
+  return <div>hello world</div>;
 }
 
 export default App;
+
